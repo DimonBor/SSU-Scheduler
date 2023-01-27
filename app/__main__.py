4caec0b42c216908284568cff25956f1af12e683
= <=>
2023-01-27 16:04:09 +0200

    It's just works

--- app/__main__.py
+++ app/__main__.py
@@ -1,9 +1,9 @@
 import os
 import os.path
 import json
-import pprint
 import requests
 import datetime
+import dateutil.tz as dtz
 from dateutil.relativedelta import relativedelta
 
 from google.auth.transport.requests import Request
@@ -14,16 +14,18 @@ from googleapiclient.errors import HttpError
 
 requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = 'ALL:@SECLEVEL=1'
 
-GROUP_CODE = os.getenv('GROUP_CODE')
+SCHEDULE_PERIOD = int(os.getenv('SCHEDULE_PERIOD'))
+GROUP_CODE = int(os.getenv('GROUP_CODE'))  # 1002732
+POPUP_REMINDER = int(os.getenv('POPUP_REMINDER'))
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 
 def getSchedule():
     Data = {
-        "id_grp": GROUP_CODE,  # 1002732
+        "id_grp": GROUP_CODE,
         "date_beg": (datetime.date.today()).strftime("%d.%m.%Y"),
         "date_end": (
-            datetime.date.today() + relativedelta(months=1)
+            datetime.date.today() + relativedelta(days=SCHEDULE_PERIOD)
         ).strftime("%d.%m.%Y")
     }
 
@@ -39,40 +41,75 @@ def getSchedule():
 
 def updateEvents(creds):
     scheduleJSON = getSchedule()
-    
+
     try:
         service = build('calendar', 'v3', credentials=creds)
 
         for ssuEvent in scheduleJSON:
-            
-            events_result = service.events().list(calendarId='primary', timeMin=now,
-                                                maxResults=10, singleEvents=True,
-                                                orderBy='startTime').execute()
-            events = events_result.get('items', [])
+            if not ssuEvent['NAME_DISC']: continue
 
-            if not events:
-                print('No upcoming events found.')
-                return
+            timeStart = datetime.datetime.strptime(
+                f"{ssuEvent['DATE_REG']} {ssuEvent['TIME_PAIR'][:5]}",
+                "%d.%m.%Y %H:%M"
+            ).replace(tzinfo=dtz.gettz("Europe/Kyiv")).isoformat()
 
-            # Prints the start and name of the next 10 events
-            for event in events:
-                start = event['start'].get('dateTime', event['start'].get('date'))
-                print(start, event['summary'])
+            timeEnd = datetime.datetime.strptime(
+                f"{ssuEvent['DATE_REG']} {ssuEvent['TIME_PAIR'][6:]}",
+                "%d.%m.%Y %H:%M"
+            ).replace(tzinfo=dtz.gettz("Europe/Kyiv")).isoformat()
+
+            checkEventsResult = service.events().list(      # Check if events
+                calendarId='primary',                       # already created
+                timeMin=timeStart,
+                timeMax=timeEnd,
+                singleEvents=True,
+                orderBy='startTime'
+            ).execute()
+
+            events = checkEventsResult.get('items', [])
+
+            if events:
+                continue
+
+            event = {
+                'summary': ssuEvent['NAME_DISC'],
+                'location': ssuEvent['NAME_AUD'] if ssuEvent['NAME_AUD'] else 'Online',
+                'description': ssuEvent['NAME_FIO'],
+                'start': {
+                    'dateTime': timeStart,
+                    'timeZone': 'Europe/Kyiv',
+                },
+                'end': {
+                    'dateTime': timeEnd,
+                    'timeZone': 'Europe/Kyiv',
+                },
+                'recurrence': None,
+                'reminders': {
+                    'useDefault': False,
+                    'overrides': [
+                        {'method': 'popup', 'minutes': POPUP_REMINDER},
+                    ],
+                },
+            }
+
+            event = service.events().insert(
+                calendarId='primary',
+                body=event
+            ).execute()
+
+            print('Event created: %s' % (event.get('htmlLink')))
 
     except HttpError as error:
         print('An error occurred: %s' % error)
 
+
 def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
+
+    # Stolen from Google examples
     creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
+
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -80,11 +117,12 @@ def main():
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
+
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
-    getSchedule()
+    updateEvents(creds)
+
 
 if __name__ == '__main__':
     main()
